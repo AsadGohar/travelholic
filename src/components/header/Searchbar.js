27fd9cfc9aa823e549dede102a4fda945159c9a4
Shah Faisal <75284252+shahfaisallive@users.noreply.github.com>
2021-01-13 23:05:03 +0500

    searchbar/footer modified

--- src/components/header/Searchbar.js
+++ src/components/header/Searchbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./Searchbar.css";
 
+//LALALALALALAl
+
 function Searchbar() {
     return (
-        <div className="row container-fluid " id="footer-wrap">
-            <div className="col-md-12 d-flex justify-content-center">
-                <form class="form-inline active-green-3 active-green-4">
+        <div className="row container-fluid d-flex justify-content-center " id="searchbar-wrap">
+            <div className="col input-group ">
+                <form class="w-100 d-flex justify-content-center form-inline active-green-3 active-green-4">
                     <i class="fa fa-search" aria-hidden="true" style={{ color: "#114B5F", fontSize: 18 }}></i>
                     <input class="form-control form-control-sm ml-3 " style={{ width: 400, height: 35 }} type="text" placeholder="Search your destination" aria-label="Search" />
-                    <button type="submit" id="search-btn">Search</button>
+                    <button type="button" id="search-btn">Search</button>
                 </form>
             </div>
         </div>
@@ -17,3 +19,6 @@ function Searchbar() {
 
 
 export default Searchbar;
+
+
+

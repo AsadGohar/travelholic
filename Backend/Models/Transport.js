6b0551314863110abf83ef359c52248e37246b68
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-02-01 03:03:31 +0500

    models schema update
    changed file and schema name Tour to Trip, updated user,transport,destinations schemas, added hotel schema, deleted company schema

--- Backend/Models/Transport.js
+++ Backend/Models/Transport.js
@@ -3,8 +3,7 @@ const mongoose = require ('mongoose');
 const TransportSchema = mongoose.Schema({
     name: { type: String , required:true},
     route:{type : mongoose.Schema.ObjectId, ref : 'Route'},
-    best:{type:Number,required:true},
-    low_budget:{type:Number,required:true}
+    fare:{type:Number,required:true},
 },{ timestamps: true});
 
 const TansportModel = mongoose.model('Transport', TransportSchema);

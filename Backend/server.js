b8625a25172e507d48240b4cb517d312e2837005
Shah Faisal <75284252+shahfaisallive@users.noreply.github.com>
2021-02-06 15:34:46 +0500

    bcrypt issue fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 const express = require ('express');
 const dotenv = require("dotenv")
 const HttpError = require('./Models/HttpError');
-const app = express();
 const connectDB = require('./config/db')
 
 

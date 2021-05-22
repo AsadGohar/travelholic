94a3608c4d11f869c92520efd0d1941381b8f02b
Shah Faisal <75284252+shahfaisallive@users.noreply.github.com>
2021-05-22 06:02:20 +0500

    path change

--- src/components/support-components/axios.js
+++ src/components/support-components/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'https://travelogic.herokuapp.com/api'
-    baseURL: 'https://omniscient-silent-eel.glitch.me/api'
+    baseURL: 'https://travelogic.glitch.me/api'
 
 });
 
 // export const imagePath = 'https://travelogic.herokuapp.com/uploads'
-export const imagePath = 'https://omniscient-silent-eel.glitch.me/uploads'
+export const imagePath = 'https://travelogic.glitch.me/uploads'
 
 
 

a138ab055dc599030479e3f8a71cb30233fd1a49
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-06-09 16:25:16 +0500

    email confirmation after signup implemented
    added loader to sign up, changed sign up api call, created email confirmation page

--- src/components/support-components/PageNotFound.js
+++ src/components/support-components/PageNotFound.js
@@ -3,13 +3,15 @@ import {Link} from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <div className="container jumbotron mt-4">
+    <div className='pt-4'>
+      <div className="container jumbotron mt-4">
       <h1 className="display-4">404 Page Not Found!</h1>
       <p className="lead">The page you are trying to access is not available right now. </p>
       <hr className="my-4"/>
       <p className="lead">
-      <Link className="btn btn-primary btn-lg" to ="/" role="button">Back To Home</Link>
+        <Link style={{ backgroundColor: "#114b5f","color":"white" }} className="btn btn-lg" to ="/" role="button">Back To Home</Link>
       </p>
+      </div>
     </div>
   )
 }
